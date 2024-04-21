2d0bd8ebbb5b36460398147e1ba043dc235050e0
NaumDeveloper <vov4.games@yandex.ru>
2024-04-21 15:04:43 +0300

    muutos

--- ch00/example/STM32/STM32_example.c
+++ ch00/example/STM32/STM32_example.c
@@ -1,35 +1,37 @@
 
-void blink(){
+void blink()
+{
 
      //HAL USE
-	 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-	 HAL_Delay(100);
-	 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-	 HAL_Delay(100);
+         //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+         //HAL_Delay(100);
+         //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+         //HAL_Delay(100);
+      //CMSIS USE
+        //GPIOA->BSRR = GPIO_PIN_5;  //ON
+        //HAL_Delay(100);
+        //GPIOA->BSRR = GPIO_PIN_5<<16; //OFF
+        //HAL_Delay(100);
 
+      GPIOA->BSRR = 0x0020; //ON
+      HAL_Delay(100);
+      GPIOA->BSRR = 0x200000; //OFF
+      HAL_Delay(100);
 }
 
-void blink2(){
+void but1()
+{
 
 
-	// GPIOA->BSRR = GPIO_PIN_5;  //ON
-	 GPIOA->BSRR = 0x0020; //ON
-	 HAL_Delay(100);
-	// GPIOA->BSRR = GPIO_PIN_5<<16; //OFF
-	 GPIOA->BSRR = 0x200000; //OFF
-	 HAL_Delay(100);
-
-}
-
-void but1(){
-
-
-	  if(GPIOC->IDR & GPIO_PIN_13){
+	  if(GPIOC->IDR & GPIO_PIN_13)
+	  {
 		  // HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)
 
 		 // HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
 		  GPIOA->BSRR = GPIO_PIN_5;
-	  }else{
+	  }
+	  else
+	  {
 
 		//  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
 		  GPIOA->BSRR = GPIO_PIN_5<<16;
@@ -39,7 +41,8 @@ void but1(){
 }
 
 
-void but2(){
+void but2()
+{
 	
 	#include "stdbool.h"
 	
@@ -51,29 +54,28 @@ void but2(){
 
 
 	 
-	 if((GPIOC->IDR & GPIO_PIN_13)&& !flag_block1){
+	 if((GPIOC->IDR & GPIO_PIN_13)&& !flag_block1)
+	 {
 
      		  flag_block1 = 1;
      		  flag_set1 = !flag_set1;
 
-     		 if(flag_set1){
-
+     		 if(flag_set1)
+     		 {
      			 //GPIOA->BSRR = GPIO_PIN_5; //ON
      			  GPIOA->BSRR = 0x0020; // 0b0000000000100000 ON
-
-     		 }else if(!flag_set1){
-
-     		   // GPIOA->BSRR = GPIO_PIN_5<<16;
-     		    GPIOA->BSRR = 0x200000; // 0b0000 0000 0010 0000 << 16  = (0x200000) (0b1000000000000000000000) 0FF
-
-
      		 }
-
-     	  }else if(!(GPIOC->IDR & GPIO_PIN_13)&& flag_block1){
+     		 else if(!flag_set1)
+     		 {
+     		   //GPIOA->BSRR = GPIO_PIN_5<<16;
+     		    GPIOA->BSRR = 0x200000; // 0b0000 0000 0010 0000 << 16  = (0x200000) (0b1000000000000000000000) 0FF
+     		 }
+     		 else if(!(GPIOC->IDR & GPIO_PIN_13)&& flag_block1)
+     		 {
      		  flag_block1 = 0;
-     	  }
+     	     }
 	
-}
+    }
 
 /*   UART   */
 
