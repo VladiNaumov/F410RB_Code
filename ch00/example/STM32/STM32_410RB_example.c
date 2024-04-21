824e078b18000d2ba5eaa46f2f0dc0d4161283a8
NaumDeveloper <vov4.games@yandex.ru>
2024-04-21 15:15:36 +0300

    muutos

--- ch00/example/STM32/STM32_410RB_example.c
+++ ch00/example/STM32/STM32_410RB_example.c
@@ -1,7 +1,6 @@
 
 void blink()
 {
-
      //HAL USE
          //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
          //HAL_Delay(100);
@@ -23,29 +22,21 @@ void blink()
 
 void but1()
 {
-
-
 	  if(GPIOC->IDR & GPIO_PIN_13)
 	  {
-		  // HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)
-
+		 // HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)
 		 // HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
 		  GPIOA->BSRR = GPIO_PIN_5;
 	  }
 	  else
 	  {
-
 		//  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
 		  GPIOA->BSRR = GPIO_PIN_5<<16;
-
 	  }
-
 }
-
 
 void but2()
 {
-	
 	#include "stdbool.h"
 	
 	bool flag, flag_block1, flag_set1;
@@ -54,11 +45,8 @@ void but2()
 	flag_block1 = 0;
 	flag_set1 = 0;
 
-
-	 
 	 if((GPIOC->IDR & GPIO_PIN_13)&& !flag_block1)
 	 {
-
      		  flag_block1 = 1;
      		  flag_set1 = !flag_set1;
 
@@ -76,7 +64,6 @@ void but2()
      		 {
      		  flag_block1 = 0;
      	     }
-	
     }
 
 /*   UART   */
@@ -103,68 +90,52 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
 			HAL_UART_Transmit_IT(&huart2, (uint8_t*) "ERROR_Callback - Parity error \n", 29);
 			__HAL_UART_CLEAR_PEFLAG(&huart2);
 		}
-
 		if(error & HAL_UART_ERROR_NE)
 		{
 			HAL_UART_Transmit_IT(&huart2, (uint8_t*) "ERROR_Callback - Noise error \n", 28);
 			__HAL_UART_CLEAR_NEFLAG(&huart2);
 		}
-
 		if(error & HAL_UART_ERROR_FE)
 		{
 			HAL_UART_Transmit_IT(&huart2, (uint8_t*) "ERROR_Callback - Frame error \n", 28);
 			__HAL_UART_CLEAR_FEFLAG(&huart2);
 		}
-
 		if(error & HAL_UART_ERROR_ORE)
 		{
 			HAL_UART_Transmit_IT(&huart2, (uint8_t*) "ERROR_Callback - Overrun error \n", 28);
 			__HAL_UART_CLEAR_OREFLAG(&huart2);
 
 		}
-
 		if(error & HAL_UART_ERROR_DMA)
 		{
 			HAL_UART_Transmit_IT(&huart2, (uint8_t*) "ERROR_Callback - Overrun error \n", 28);
 			__HAL_UART_CLEAR_NEFLAG(&huart2);
 		}
-
 		huart ->ErrorCode = HAL_UART_ERROR_NONE;
-
-
 	}
 
 };
 
-
 	  HAL_Delay(100);
 	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-	  
 
 /* UART&USART*/
 uint8_t rx_buff[10]; // буфер (массив) на прием данных
 uint8_t tx_buff[]={0,1,2,3,4,5,6,7,8,9}; // буфер (массив) на передачу данных
-
 
   void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart);
   HAL_UART_Receive_IT(&huart1, rx_buff, 10); // прием данных
   HAL_UART_Transmit_IT(&huart1, tx_buff, 10); // передачу данных
 
 
-
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-
 	__NOP();
-
-
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
 {
 	__NOP();
-
-
 }
 
  // сканирование I2C порта на наличие подключения 
